5375b986ae55832f90e5a506667dde9748e37502
Nomaan Ahmad <94966523+nomaannn@users.noreply.github.com>
2023-03-30 07:36:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
-//your code here
+const bands = [
+    "The Plot in You",
+    "The Devil Wears Prada",
+    "Pierce the Veil",
+    "Norma Jean",
+    "The Bled",
+    "Say Anything",
+    "The Midway State",
+    "We Came as Romans",
+    "Counterparts",
+    "Oh, Sleeper",
+    "A Skylit Drive",
+    "Anywhere But Here",
+    "An Old Dog"
+];
 
+/**
+ * 我們希望排序的時候能略過一些關鍵字
+ **/
 function strip (word) {
     let regex = new RegExp('^(a |the |an )', 'i')
     return word.replace(regex, '').trim()
 }
 
 const sortedBands = bands.sort((a, b) => (strip(a) > strip(b)) ? 1 : -1)
+
+
+/** 
+ * add ul list in HTML
+ **/
+document.querySelector('#bands').innerHTML = 
+    sortedBands.map(item => `<li>${item}</li>`).join('')
