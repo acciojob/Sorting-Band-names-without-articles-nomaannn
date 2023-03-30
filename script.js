c65289353f3857f16050a63d1e6698c2fdcfb27e
Nomaan Ahmad <94966523+nomaannn@users.noreply.github.com>
2023-03-30 07:34:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 //your code here
 
+function strip (word) {
+    let regex = new RegExp('^(a |the |an )', 'i')
+    return word.replace(regex, '').trim()
+}
+
+const sortedBands = bands.sort((a, b) => (strip(a) > strip(b)) ? 1 : -1)
